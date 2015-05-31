2c7955861228cf7fa92d587229397a3e5188a247
Sam Platt <samuel.j.platt@gmail.com>
2015-05-30 23:56:12 -0400

    Schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150525214413) do
+ActiveRecord::Schema.define(version: 20150525222314) do
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   limit: 4,     default: 0, null: false
